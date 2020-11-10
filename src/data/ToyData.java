aef2e27a0d2a6de69a1125700e9429c5ec7ae923
TodSTP <72536556+TodSTP@users.noreply.github.com>
2020-11-10 16:29:56 +0700

    First version Parcels

--- src/data/ToyData.java
+++ src/data/ToyData.java
@@ -2,7 +2,9 @@ package data;
 
 import base.Bike;
 import base.Parcel;
+import base.ParcelStatus;
 import base.Person;
+import base.Location;
 
 public class ToyData {
 
@@ -16,14 +18,22 @@ public class ToyData {
     public static Person[] getPerson() {
         Person[] person = new Person[3];
         // your code
-
+        
         return person;
     }
 
     public static Parcel[] getParcels() {
         Parcel[] parcels = new Parcel[10];
-        // your code
-
+        parcels[0]=new Parcel(20.0,ParcelStatus.DELIVERED,new Location(13.00, 46.4));
+        parcels[1]=new Parcel(15.0,ParcelStatus.SHIPPING,new Location(15.00, 49.8));
+        parcels[2]=new Parcel(25.0,ParcelStatus.SHIPPING,new Location(13.90, 43.9));
+        parcels[3]=new Parcel(20.0,ParcelStatus.DELIVERED,new Location(16.30, 51.9));
+        parcels[4]=new Parcel(20.0,ParcelStatus.DELIVERED,new Location(12.00, 49.9));
+        parcels[5]=new Parcel(45.0,ParcelStatus.READY_TO_SHIP,new Location(13.50, 50.0));
+        parcels[6]=new Parcel(30.0,ParcelStatus.READY_TO_SHIP,new Location(20.00, 49.9));
+        parcels[7]=new Parcel(10.0,ParcelStatus.DELIVERED,new Location(13.20, 49.9));
+        parcels[8]=new Parcel(18.0,ParcelStatus.SHIPPING,new Location(17.90, 67.0));
+        parcels[9]=new Parcel(20.0,ParcelStatus.DELIVERED,new Location(13.00, 42.0));
         return parcels;
     }
 
